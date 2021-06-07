67427e6d0ce30ffd0d7ee46751ffa8e3dd7bfb28
drad <robertjdstoddard@gmail.com>
2021-06-07 15:17:58 -0600

    click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-var coinApiPrefix = "https://api.coinpaprika.com/v1/"
-var exchangeApiKey = "https://v6.exchangerate-api.com/v6/&f0106a145afb8388dcf1f3d0/latest/"
+var coinApiPrefix = "https://api.coinpaprika.com/v1/coins"
+var exchangeApiKey = "https://v6.exchangerate-api.com/v6/&f0106a145afb8388dcf1f3d0"
 
+//search token
+$("button").on("click", function(event){
+    // var search = this.closest("input").val()
+    console.log((event.target).closest("input"));
+
+    fetch(coinApiPrefix).then(function(response){
+        response.json().then(function(data){
+            // $("#coin-name-1").text(data)
+        })
+    })
+})
+
+//display name
