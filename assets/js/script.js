cd11f39f21cdfb6e138ea73be09174a1d02baa34
drad <robertjdstoddard@gmail.com>
2021-06-08 20:28:19 -0600

    minor styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ var exchangeApiKey = "https://v6.exchangerate-api.com/v6/&f0106a145afb8388dcf1f3
 //search token 1
 $("#button-1").click(function(){
     //search term value
-    var stuff = $("#search-1").val()
+    var searchTerm = $("#search-1").val()
     //get data value
-    fetch(coinApiPrefix + "search?q=" + stuff).then(function(response){
+    fetch(coinApiPrefix + "search?q=" + searchTerm).then(function(response){
         response.json().then(function(data){
             //set name 1
             $("#coin-name-1").text(data.currencies[0].name)
@@ -31,9 +31,9 @@ $("#button-1").click(function(){
 //search token 2
 $("#button-2").click(function(){
     //search term value
-    var stuff = $("#search-2").val()
+    var searchTerm = $("#search-2").val()
     //get data from api
-    fetch(coinApiPrefix + "search?q=" + stuff).then(function(response){
+    fetch(coinApiPrefix + "search?q=" + searchTerm).then(function(response){
         response.json().then(function(data){
             //set name 2
             $("#coin-name-2").text(data.currencies[0].name)
