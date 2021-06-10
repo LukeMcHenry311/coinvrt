5f0cf540299527c175b35f866f34245ca5b4e180
drad <robertjdstoddard@gmail.com>
2021-06-09 18:09:17 -0600

    national treasury

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,53 @@ $("#button-1").click(function(){
             //get coin price
             fetch(coinApiPrefix + "tickers/" + data.currencies[0].id).then(function(response){
                 response.json().then(function(data){
+                    //coin data
                     var convert = data.quotes.USD.price
-                    fetch('https://v6.exchangerate-api.com/v6/2b123b2e2008af090ae16479/latest/USD')
-                    .then(response => response.json())
-                    .then(data => console.log(data));
+                    //curency symbol table
+                    var currencySymbol = ""
+                    switch($("#currency-1").val()){
+                        case "USD":
+                            currencySymbol = "$"
+                            break;
+                        case "EUR":
+                            currencySymbol = "&#128;"
+                            break;
+                        case "JPY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "GBP":
+                            currencySymbol = "&#163;"
+                            break;
+                        case "AUD":
+                            currencySymbol = "A$"
+                            break;
+                        case "CAD":
+                            currencySymbol = "C$"
+                            break;
+                        case "CHF":
+                            currencySymbol = "&#8355;"
+                            break;
+                        case "CNY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "SEK":
+                            currencySymbol = "kr"
+                            break;
+                        case "NZD":
+                            currencySymbol = "$"
+                            break;
+                    }
+                    //exchange api call
+                    fetch('https://v6.exchangerate-api.com/v6/2b123b2e2008af090ae16479/pair/USD/' + $("#currency-1").val())
+                        .then(response => response.json())
+                        .then(function(data){
+                            //set conversion info
+                            $("#rate-1").html(currencySymbol + (data.conversion_rate * convert).toFixed(2))
+                        });
                     //price data display
-                    $("#price-1").text("Current Price: $" + (data.quotes.USD.price).toFixed(2))
+                    $("#price-1").text("Current Price: $" + convert.toFixed(2))
+                    //up or down arrow
+                    $("#arrow-1").removeClass()
                     if(data.quotes.USD.percent_change_24h > 0){
                         $("#arrow-1").addClass("oi oi-arrow-circle-top")
                     }
@@ -46,15 +87,176 @@ $("#button-2").click(function(){
             //get price info
             fetch(coinApiPrefix + "tickers/" + data.currencies[0].id).then(function(response){
                 response.json().then(function(data){
+                    //coin data
+                    var convert = data.quotes.USD.price
+                    //curency symbol table
+                    var currencySymbol = ""
+                    switch($("#currency-2").val()){
+                        case "USD":
+                            currencySymbol = "$"
+                            break;
+                        case "EUR":
+                            currencySymbol = "&#128;"
+                            break;
+                        case "JPY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "GBP":
+                            currencySymbol = "&#163;"
+                            break;
+                        case "AUD":
+                            currencySymbol = "A$"
+                            break;
+                        case "CAD":
+                            currencySymbol = "C$"
+                            break;
+                        case "CHF":
+                            currencySymbol = "&#8355;"
+                            break;
+                        case "CNY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "SEK":
+                            currencySymbol = "kr"
+                            break;
+                        case "NZD":
+                            currencySymbol = "$"
+                            break;
+                    }
+                    //exchange api call
+                    fetch('https://v6.exchangerate-api.com/v6/2b123b2e2008af090ae16479/pair/USD/' + $("#currency-2").val())
+                        .then(response => response.json())
+                        .then(function(data){
+                            //set conversion info
+                            $("#rate-2").html(currencySymbol + (data.conversion_rate * convert).toFixed(2))
+                        });
                     //display price info
                     $("#price-2").text("Current Price: $" + (data.quotes.USD.price).toFixed(2))
+                    //up or down arrow
+                    $("#arrow-2").removeClass()
                     if(data.quotes.USD.percent_change_24h > 0){
                         $("#arrow-2").addClass("oi oi-arrow-circle-top")
                     }
                     else if(data.quotes.USD.percent_change_24h < 0){
-                        stuff = 4
                         $("#arrow-2").addClass("oi oi-arrow-circle-bottom")
                     }
+                })
+            })
+        })
+    })
+})
+
+//update when an option is chosen
+$("#currency-1").change(function(){
+    //search term value
+    var searchTerm = $("#search-1").val()
+    //get data value
+    fetch(coinApiPrefix + "search?q=" + searchTerm).then(function(response){
+        response.json().then(function(data){
+            //get coin price
+            fetch(coinApiPrefix + "tickers/" + data.currencies[0].id).then(function(response){
+                response.json().then(function(data){
+                    //coin data
+                    var convert = data.quotes.USD.price
+                    //curency symbol table
+                    var currencySymbol = ""
+                    switch($("#currency-1").val()){
+                        case "USD":
+                            currencySymbol = "$"
+                            break;
+                        case "EUR":
+                            currencySymbol = "&#128;"
+                            break;
+                        case "JPY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "GBP":
+                            currencySymbol = "&#163;"
+                            break;
+                        case "AUD":
+                            currencySymbol = "A$"
+                            break;
+                        case "CAD":
+                            currencySymbol = "C$"
+                            break;
+                        case "CHF":
+                            currencySymbol = "&#8355;"
+                            break;
+                        case "CNY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "SEK":
+                            currencySymbol = "kr"
+                            break;
+                        case "NZD":
+                            currencySymbol = "$"
+                            break;
+                    }
+                    //exchange api call
+                    fetch('https://v6.exchangerate-api.com/v6/2b123b2e2008af090ae16479/pair/USD/' + $("#currency-1").val())
+                        .then(response => response.json())
+                        .then(function(data){
+                            //set conversion info
+                            $("#rate-1").html(currencySymbol + (data.conversion_rate * convert).toFixed(2))
+                    });
+                })
+            })
+        })
+    })
+})
+
+$("#currency-2").change(function(){
+    //search term value
+    var searchTerm = $("#search-2").val()
+    //get data value
+    fetch(coinApiPrefix + "search?q=" + searchTerm).then(function(response){
+        response.json().then(function(data){
+            //get coin price
+            fetch(coinApiPrefix + "tickers/" + data.currencies[0].id).then(function(response){
+                response.json().then(function(data){
+                    //coin data
+                    var convert = data.quotes.USD.price
+                    //curency symbol table
+                    var currencySymbol = ""
+                    switch($("#currency-2").val()){
+                        case "USD":
+                            currencySymbol = "$"
+                            break;
+                        case "EUR":
+                            currencySymbol = "&#128;"
+                            break;
+                        case "JPY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "GBP":
+                            currencySymbol = "&#163;"
+                            break;
+                        case "AUD":
+                            currencySymbol = "A$"
+                            break;
+                        case "CAD":
+                            currencySymbol = "C$"
+                            break;
+                        case "CHF":
+                            currencySymbol = "&#8355;"
+                            break;
+                        case "CNY":
+                            currencySymbol = "&#165;"
+                            break;
+                        case "SEK":
+                            currencySymbol = "kr"
+                            break;
+                        case "NZD":
+                            currencySymbol = "$"
+                            break;
+                    }
+                    //exchange api call
+                    fetch('https://v6.exchangerate-api.com/v6/2b123b2e2008af090ae16479/pair/USD/' + $("#currency-2").val())
+                        .then(response => response.json())
+                        .then(function(data){
+                            //set conversion info
+                            $("#rate-2").html(currencySymbol + (data.conversion_rate * convert).toFixed(2))
+                    });
                 })
             })
         })
